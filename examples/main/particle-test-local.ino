8274af4ddc17a9a8cfa8101a30a1f3075aab71ad
asingularity <csaba.petre@gmail.com>
2022-04-25 08:24:02 -0700

    minimal storage of string testing

--- examples/main/particle-test-local.ino
+++ examples/main/particle-test-local.ino
@@ -8,44 +8,71 @@
 
 
 #include "Particle.h"
-#include "softap_http.h"
-#include "hotspot-http-server.h"
+//#include "softap_http.h"
+//#include "hotspot-http-server.h"
 
 // new
 
-#include "config-manager.h"
-#include "game-manager.h"
+//#include "config-manager.h"
+//#include "game-manager.h"
 
 // classes
-HubInterface hub;
-GameManager gameMan(&hub);    // which game to play given gameId from config
-ConfigManager configMan(&hub, &gameMan);  // handles config of hub and game via webpage and eeprom
+//HubInterface hub;
+//GameManager gameMan(&hub);    // which game to play given gameId from config
+//ConfigManager configMan(&hub, &gameMan);  // handles config of hub and game via webpage and eeprom
 
 // enables simultaneous execution of application and system thread
 SYSTEM_THREAD(ENABLED);
 
 // hotspot
-STARTUP(softap_set_application_page_handler(myPages, nullptr));
+//vSTARTUP(softap_set_application_page_handler(myPages, nullptr));
+long int last_time;
+bool test_done = false;
+
+const char index_html[] = "<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><title>Setup your device</title><link rel='stylesheet' type='text/css' href='style.css'></head><body><h1>Connect me to your WiFi!</h1><h3>My device ID:</h3><input type=text id='device-id' size='25' value='' disabled/><button type='button' class='input-helper' id='copy-button'>Copy</button><div id='scan-div'><h3>Scan for visible WiFi networks</h3><button id='scan-button' type='button'>Scan</button></div><div id='networks-div'></div><div id='connect-div' style='display: none'><p>Don't see your network? Move me closer to your router, then re-scan.</p><form id='connect-form'><input type='password' id='password' size='25' placeholder='password'/><button type='button' class='input-helper' id='show-button'>Show</button><button type='submit' id='connect-button'>Connect</button></form></div><script src='rsa-utils/jsbn_1.js'></script><script src='rsa-utils/jsbn_2.js'></script><script src='rsa-utils/prng4.js'></script><script src='rsa-utils/rng.js'></script><script src='rsa-utils/rsa.js'></script><script src='script.js'></script></body></html><!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><title>Setup your device</title><link rel='stylesheet' type='text/css' href='style.css'></head><body><h1>Connect me to your WiFi!</h1><h3>My device ID:</h3><input type=text id='device-id' size='25' value='' disabled/><button type='button' class='input-helper' id='copy-button'>Copy</button><div id='scan-div'><h3>Scan for visible WiFi networks</h3><button id='scan-button' type='button'>Scan</button></div><div id='networks-div'></div><div id='connect-div' style='display: none'><p>Don't see your network? Move me closer to your router, then re-scan.</p><form id='connect-form'><input type='password' id='password' size='25' placeholder='password'/><button type='button' class='input-helper' id='show-button'>Show</button><button type='submit' id='connect-button'>Connect</button></form></div><script src='rsa-utils/jsbn_1.js'></script><script src='rsa-utils/jsbn_2.js'></script><script src='rsa-utils/prng4.js'></script><script src='rsa-utils/rng.js'></script><script src='rsa-utils/rsa.js'></script><script src='script.js'></script></body></html><!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><title>Setup your device</title><link rel='stylesheet' type='text/css' href='style.css'></head><body><h1>Connect me to your WiFi!</h1><h3>My device ID:</h3><input type=text id='device-id' size='25' value='' disabled/><button type='button' class='input-helper' id='copy-button'>Copy</button><div id='scan-div'><h3>Scan for visible WiFi networks</h3><button id='scan-button' type='button'>Scan</button></div><div id='networks-div'></div><div id='connect-div' style='display: none'><p>Don't see your network? Move me closer to your router, then re-scan.</p><form id='connect-form'><input type='password' id='password' size='25' placeholder='password'/><button type='button' class='input-helper' id='show-button'>Show</button><button type='submit' id='connect-button'>Connect</button></form></div><script src='rsa-utils/jsbn_1.js'></script><script src='rsa-utils/jsbn_2.js'></script><script src='rsa-utils/prng4.js'></script><script src='rsa-utils/rng.js'></script><script src='rsa-utils/rsa.js'></script><script src='script.js'></script></body></html><!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><title>Setup your device</title><link rel='stylesheet' type='text/css' href='style.css'></head><body><h1>Connect me to your WiFi!</h1><h3>My device ID:</h3><input type=text id='device-id' size='25' value='' disabled/><button type='button' class='input-helper' id='copy-button'>Copy</button><div id='scan-div'><h3>Scan for visible WiFi networks</h3><button id='scan-button' type='button'>Scan</button></div><div id='networks-div'></div><div id='connect-div' style='display: none'><p>Don't see your network? Move me closer to your router, then re-scan.</p><form id='connect-form'><input type='password' id='password' size='25' placeholder='password'/><button type='button' class='input-helper' id='show-button'>Show</button><button type='submit' id='connect-button'>Connect</button></form></div><script src='rsa-utils/jsbn_1.js'></script><script src='rsa-utils/jsbn_2.js'></script><script src='rsa-utils/prng4.js'></script><script src='rsa-utils/rng.js'></script><script src='rsa-utils/rsa.js'></script><script src='script.js'></script></body></html>";
+String new_str;
+
 
 // setup() runs once, when the device is first turned on.
 void setup() {
-
-    configMan.Initialize();  // also initializes game manager
-    hub.Initialize("game_ID_here_TODO");
+    last_time = millis();
+    //configMan.Initialize();  // also initializes game manager
+    //hub.Initialize("game_ID_here_TODO");
     // in games it was: hub.Initialize(__FILE__);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
 
+    if ((millis() - last_time) > 5000)
+    {
+        Log.info("Free memory:");
+        Serial.println(System.freeMemory());
+        last_time = millis();
+    }
+
+    if (millis() > 15000 && !test_done)
+    {
+        Log.info("Free memory BEFORE:");
+        Serial.println(System.freeMemory());
+        
+        Serial.println();
+        
+        new_str = index_html;
+
+        Log.info("Free memory AFTER:");
+        Serial.println(System.freeMemory());
+
+        test_done = true;
+    }
     // serve webpage, read/write eeprom as config changes
-    configMan.Run();
+    //configMan.Run();
     
     // ************************************ DISABLE FOR TESTING WITHOUT HUB ************************************
     // run the hub
-    hub.Run(20);
+    // hub.Run(20);
     // ************************************ ************************************ ************************************
 
     // run the loop for the current active game
-    gameMan.Run();
+    //gameMan.Run();
 }
